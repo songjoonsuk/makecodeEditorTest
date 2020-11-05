fbb1dfb69ecf9147a099e202ef7815986c3362a4
songjoonsuk <52555877+songjoonsuk@users.noreply.github.com>
2020-11-05 16:47:58 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 namespace MagicShow {
+
+
+    //% blockId = kkk
+    //% block = "plot at %index" 
+    //% index.min = 0  index.max=25
     export function PlotAt ( index : number) : void {
         let y = Math.floor( index / 5 );
         let x = Math.floor( index % 5 );
