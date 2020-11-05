3734c3dc39dc3c1cc380ce1164f311a309c84144
songjoonsuk <52555877+songjoonsuk@users.noreply.github.com>
2020-11-05 16:38:48 +0900

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+namespace MagicShow {
+    export function PlotAt ( index : number) : void {
+        let y = Math.floor( index / 5 );
+        let x = Math.floor( index % 5 );
+        led.plot(x,y);
+    } 
+
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+for( let i=0; i< 25 ; i++){
+    MagicShow.PlotAt(i)
+    basic.pause(300);
+}    
